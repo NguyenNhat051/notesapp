bbf9f1a82f8bbed709332b9565690a3e1e6d3cf6
NguyenNhat <nguyennhat669903@gmail.com>
2022-10-23 03:25:11 +0700

    modified:   package-lock.json     modified:   package.json 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'react-mde/lib/styles/css/react-mde-all.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
